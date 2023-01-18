00e7a2bec0348219afbbe78c59ef61c41c888c34
Ellen Bennett <bennette831@gmail.com>
2023-01-17 19:47:32 -0700

    passing lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName () {
+   customerName = 'BOB';
+   return customerName;
+
+};
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+};
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+};
+
+const leastFavoriteCustomer = 'renee';
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'chad';
+};
